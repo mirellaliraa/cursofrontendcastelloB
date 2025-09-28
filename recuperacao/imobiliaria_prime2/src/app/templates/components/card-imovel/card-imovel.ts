b2f84a131b43668e4ddb30628e643bf75f18e5d0
mirellaliraa <mirellalira0801@gmail.com>
2025-09-27 22:36:42 -0300

    27-09-2025

--- recuperacao/imobiliaria_prime2/src/app/templates/components/card-imovel/card-imovel.ts
+++ recuperacao/imobiliaria_prime2/src/app/templates/components/card-imovel/card-imovel.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Imovel } from '../../../core/models/imovel.model';
 import { Imoveis } from '../../../core/services/imoveis';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 import { InteresseService } from '../../../core/services/interesse';
 import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-card-imovel',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './card-imovel.html',
   styleUrl: './card-imovel.css'
 })
@@ -16,10 +16,10 @@ import { AuthService } from '../../../core/services/auth';
 export class CardImovel implements OnInit {
   imovel?: Imovel;
 
-  constructor(private imovelService: Imoveis, private route: ActivatedRoute, private interesseService: InteresseService, private auth: AuthService) { }
+  constructor(private imovelService: Imoveis, private route: ActivatedRoute, private interesseService: InteresseService, private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.imovelService.getImovelById(id).subscribe({
         next: (dados) => (this.imovel = dados),
@@ -28,19 +28,17 @@ export class CardImovel implements OnInit {
     }
   }
 
-  registrarInteresse(imovelId?: number) {
-    if (!imovelId) return;
-
-    const usuario = this.auth.usuarioAtual();
-    if (usuario && usuario.tipo === 'cliente') {
-      this.interesseService.createInteresse(usuario.id, imovelId).subscribe(() => {
-        alert('Interesse registrado com sucesso!');
-      });
-    } else {
-      alert('Apenas clientes podem marcar interesse.');
-    }
+  registrarInteresse(imovelId?: string) {
+  if (!imovelId) return;
+  const usuario = this.auth.usuarioAtual();
+  if (usuario && usuario.tipo === 'cliente') {
+    this.interesseService.createInteresse(usuario.id, imovelId).subscribe(() => {
+      alert('Interesse registrado com sucesso!');
+      this.router.navigate(['/meus-interesses']); 
+    });
+  } else {
+    alert('Apenas clientes podem marcar interesse.');
   }
-
 }
-
+}
 
